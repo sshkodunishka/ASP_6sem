b03fe29853ed01de6dfd82fe0f4a0670467ac80f
Kristina Shkoda <kristinashkoda3@gmail.com>
2023-03-05 21:16:35 +0300

    add first lab

---
+++ lab01_client/lab01_client/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace lab01_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.numberOne = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.numberTwo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numberOne
+            // 
+            this.numberOne.Location = new System.Drawing.Point(12, 12);
+            this.numberOne.Name = "numberOne";
+            this.numberOne.Size = new System.Drawing.Size(100, 22);
+            this.numberOne.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(233, 12);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 22);
+            this.result.TabIndex = 2;
+            // 
+            // numberTwo
+            // 
+            this.numberTwo.Location = new System.Drawing.Point(127, 12);
+            this.numberTwo.Name = "numberTwo";
+            this.numberTwo.Size = new System.Drawing.Size(100, 22);
+            this.numberTwo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(743, 352);
+            this.Controls.Add(this.numberTwo);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.numberOne);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox numberOne;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox numberTwo;
+    }
+}
+
---
+++ lab01_client/lab01_client/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab01_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(this.numberOne.Text, out int firstValue) && int.TryParse(this.numberTwo.Text, out int secondValue))
+            {
+
+                var data = new List<KeyValuePair<string, string>>();
+                data.Add(new KeyValuePair<string, string>("X", this.numberOne.Text));
+                data.Add(new KeyValuePair<string, string>("Y", this.numberTwo.Text));
+
+                var url = "https://localhost:44365/task4";
+                using (var client = new HttpClient())
+                {
+                    var req = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(data) };
+                    var res = await client.SendAsync(req);
+                    this.result.Text = res.Content.ReadAsStringAsync().Result; ;
+                }
+
+
+            }
+        }
+    }
+}
